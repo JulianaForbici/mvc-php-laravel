faacc8bb7ef311c0fbb0253ba32bcae71f9fab12
juliana.forbici <juliana.forbici@senior.com.br>
2026-03-17 15:26:21 -0300

    feat: adicionado seeder com o comando 'php artisan make:seeder ChirpSeeder' e adicionado no db com o comando 'php artisan db:seed --class=ChirpSeeder'

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\resource;
 use Illuminate\Http\Request;
+use App\Models\Chirp;
 
 class ChirpController extends Controller
 {
@@ -12,23 +13,7 @@ class ChirpController extends Controller
      */
    public function index()
 {
-    $chirps = [
-        [
-            'author' => 'Jane Doe',
-            'message' => 'Just deployed my first Laravel app! 🚀',
-            'time' => '5 minutes ago'
-        ],
-        [
-            'author' => 'John Smith',
-            'message' => 'Laravel makes web development fun again!',
-            'time' => '1 hour ago'
-        ],
-        [
-            'author' => 'Alice Johnson',
-            'message' => 'Working on something cool with Chirper...',
-            'time' => '3 hours ago'
-        ]
-    ];
+    $chirps = Chirp::with('user')->latest()->take(50)->get();
 
     return view('home', ['chirps' => $chirps]);
 }
