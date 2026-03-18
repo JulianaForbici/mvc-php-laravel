d0948053c6ff0fecebb0911b56f16786330924cf
JulianaForbici <julianaforbicii@gmail.com>
2026-03-17 21:53:12 -0300

    feat: adicionado login e logout funcionais

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\Auth\Register;
 use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\Login;
+use App\Http\Controllers\Auth\Logout;
 
 Route::get('/', [ChirpController::class, 'index']);
 Route::middleware('auth')->group(function () {
@@ -21,3 +23,16 @@ Route::view('/register', 'auth.register')
 
 Route::post('/register', Register::class)
      ->middleware('guest');
+
+// login routes
+Route::view('/login', 'auth.login')
+    ->middleware('guest')
+    ->name('login');
+ 
+Route::post('/login', Login::class)
+    ->middleware('guest');
+ 
+// Logout route
+Route::post('/logout', Logout::class)
+    ->middleware('auth')
+    ->name('logout');
